b0b8311a4be73048d96f480b15e801ad09aaa283
di <stevejobsii@outlook.com>
2017-05-07 03:36:41 +0800

    config

--- config/wechat.php
+++ config/wechat.php
@@ -50,6 +50,11 @@ return [
     /*
      * 微信支付
      */
+     'payment' => [
+        'merchant_id'        => env('WECHAT_PAYMENT_MERCHANT_ID', 'your-mch-id'),
+        'key'                => env('WECHAT_PAYMENT_KEY', 'key-for-signature'),
+        'cert_path'          => env('WECHAT_PAYMENT_CERT_PATH', 'path/to/your/cert.pem'), // XXX: 绝对路径！！！！
+        'key_path'           => env('WECHAT_PAYMENT_KEY_PATH', 'path/to/your/key'), 
     // 'payment' => [
     //     'merchant_id'        => env('WECHAT_PAYMENT_MERCHANT_ID', 'your-mch-id'),
     //     'key'                => env('WECHAT_PAYMENT_KEY', 'key-for-signature'),
