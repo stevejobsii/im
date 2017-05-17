62b6a12af0b0c3593d9e93574b9ece4c52c341bc
di <stevejobsii@outlook.com>
2017-05-17 15:23:26 +0800

    test admin event

---
+++ app/eventfacades/EventCommodity.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class ProductCommodity extends Model
+{
+    protected $table = 'event_commodity';
+
+    protected $guarded = [];
+
+    // public function topic(){
+    //     return $this->belongsTo('App\ProductTopic','topic_id');
+    // }
+
+    // public function plate(){
+    //     return $this->belongsTo('App\ProductPlate','plate_id');
+    // }
+    // public function category(){
+    //     return $this->belongsTo('App\ProductCategory','category_id');
+    // }
+
+    // 与event table 一对一关系
+    // public function status(){
+    //     return $this->hasone('App\Event','product_commodity_id');
+    // }
+
+}
