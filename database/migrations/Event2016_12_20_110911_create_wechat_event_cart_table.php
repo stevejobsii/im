82a9cd5841f98f13065a842eb7fbac78e470e3c9
di <stevejobsii@outlook.com>
2017-05-17 12:31:15 +0800

    CreateWechatEventCartTable

--- database/migrations/Event2016_12_20_110911_create_wechat_event_cart_table.php
+++ database/migrations/Event2016_12_20_110911_create_wechat_event_cart_table.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateEventCartTable extends Migration
+class CreateWechatEventCartTable extends Migration
 {
     /**
      * Run the migrations.
@@ -12,7 +12,7 @@ class CreateEventCartTable extends Migration
      */
     public function up()
     {
-        Schema::create('event_cart', function (Blueprint $table) {
+        Schema::create('wechat_event_cart', function (Blueprint $table) {
             $table->increments('id');
             $table->string('openid');
             $table->integer('commodity_id');
