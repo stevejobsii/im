7e38c50ca88ec25c8992b7e68716296669349034
di <stevejobsii@outlook.com>
2017-05-28 19:20:33 +0800

    any

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -100,8 +100,7 @@ Route::group(['prefix' => 'api', 'middleware' => ['web', 'wechat.oauth'], 'names
      * 根据不同条件获取活动数据集合,['报名中','已报满','已结束']
      */
     // ['报名中','已报满','已结束']活动
-    Route::post('geteventcommodities/status', 'Event\ShopController@getCommodityByStatus');
-    Route::get('geteventcommodities/status', 'Event\ShopController@getCommodityByStatus');
+    Route::any('geteventcommodities/status', 'Event\ShopController@getCommodityByStatus');
 
 
     /**
