4d37d413e5f7ddabc7f6648864e9f2ba0e2e7476
di <stevejobsii@outlook.com>
2017-06-04 11:34:01 +0800

    test {{ lang('welcome') }}

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -4,7 +4,7 @@
 <head>
 
     <meta charset="utf-8">
-    <title>微信商城 | 登录</title>
+    <title>微信商城 | 登录</title>{{ lang('welcome') }}
     <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="description" content="">
