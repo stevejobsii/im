8e07ed3589a34f16949a9ef5e5f120b6f3a1c8df
di <stevejobsii@outlook.com>
2017-06-05 18:22:07 +0800

    //Vue.prototype.trans = string => '123';

--- resources/assets/js/event/main.js
+++ resources/assets/js/event/main.js
@@ -14,7 +14,9 @@ Vue.use(Router);
 
     //vuejs using laravel blade inside
     //const _ = import('lodash');
-Vue.prototype.trans = string => _.get(window.i18n, string);
+//Vue.prototype.trans = string => '123';
+//Vue.prototype.trans = string => _.get(window.i18n, string);
+
 /**
  * 价格转换为0.00的浮点数
  */
