632ed7e80af029a46001da0a426a3de680ac5831
di <stevejobsii@outlook.com>
2017-06-09 09:49:04 +0800

    template: '<router-view></router-view>'

--- resources/assets/js/event/main.js
+++ resources/assets/js/event/main.js
@@ -145,7 +145,7 @@ router.beforeEach((transition) => {
 var vm = new Vue({
   el: 'body',
   router: router,
-  template: App
+  template: '<router-view>123</router-view>'
 });
 
 //import App from './App.vue'
